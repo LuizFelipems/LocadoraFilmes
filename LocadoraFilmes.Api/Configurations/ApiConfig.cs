57eb7809bbf7065a63e3f1d83236d277880338eb
LuizFelipems <luizfelipems12@gmail.com>
2023-09-11 07:59:19 -0300

    Modified UseSwaggerUI

--- LocadoraFilmes.Api/Configurations/ApiConfig.cs
+++ LocadoraFilmes.Api/Configurations/ApiConfig.cs
@@ -170,7 +170,11 @@ namespace LocadoraFilmes.Api.Configurations
         private static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
         {
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Api LocacaoFilmes");
+                options.RoutePrefix = string.Empty;
+            });
 
             return app;
         }
